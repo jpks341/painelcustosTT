38c44795bc902fd3bd46d772d3d57102489cb561
batmanpereira <josiaspcconta2@gmail.com>
2024-07-25 14:41:08 -0300

    Update config.php

--- Josias/ttpanel/ttpanel/include/config.php
+++ Josias/ttpanel/ttpanel/include/config.php
@@ -8,7 +8,7 @@
 
 return [
     'host' => 'localhost',
-    'db_name' => 'painel_custos', // Имя базы данных
+    'db_name' => 'painel_custo', // Имя базы данных
     'user' => 'josias', // Юзер базы данных
     'password' => 'josias456', // Пароль базы данных
     'charset' => 'utf8'
